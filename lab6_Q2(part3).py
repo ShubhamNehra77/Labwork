f40a69e964d4da74186611b4fd422596c2b9a223
ShubhamNehra77 <141896384+ShubhamNehra77@users.noreply.github.com>
2023-09-26 23:10:26 +0530

    Update lab6_Q2(part3).py

--- lab6_Q2(part3).py
+++ lab6_Q2(part3).py
@@ -6,7 +6,7 @@ if N<0 or X<0:
     print(f'Enter a positive no')
 else:
     #intializing and looping and printing output
-    sum=0
+    sum=1
     for i in range(1,N+1):
         sum=sum+((X**i)/i)
     print(f'{sum:.9f}')    
